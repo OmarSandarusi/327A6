65f5bb77da794268af5794f718524b3575356902
jonathanturcottte <jonathanturcotte@outlook.com>
2017-11-29 18:32:09 -0500

    Add in backend files, added gitignore

--- daily.sh
+++ daily.sh
@@ -1,67 +1,35 @@
 #!/bin/bash
+
+# Expects the day to be the first command line options passed to the file
+
 ORIGDIR=`pwd`
+NUMSESSIONS=3
+DAY="day$1"
+NEXTDAY=$(($1 + 1))
+INDIR="$ORIGDIR/inputs/$DAY"
+OUTDIR="$ORIGDIR/outputs/$DAY"
 
-# Run all tests in input/output directories
+
+# Run each session of the front end for the given day
 cd input
-for feature in `ls -v`
+for i in `seq $NUMSESSIONS`
 do
-echo "Testing feature: $feature"
-    cd $feature
-    for test in `ls -v`
-    do
-        echo "Running test: $test"
-        TESTDIR="$ORIGDIR/input/$feature/$test"
-        OUTDIR="$ORIGDIR/output/$feature/$test"
-        python "$ORIGDIR/main.py" "$TESTDIR/accounts.txt" "$OUTDIR/transactions.txt" \
-            < "$TESTDIR/input.txt" > "$OUTDIR/log.txt"
-    done
-    cd ../
-    echo ""
+echo "Running session $i on $DAY..."
+        python "$ORIGDIR/src/frontend/main.py" "$INDIR/accounts.txt" "$OUTDIR/transaction$i.txt" \
+            < "$INDIR/session$i.txt" > "$OUTDIR/session$i.log"
 done
 
-# Run the comparison on each test result
-cd ../
-passed=0
-failed=0
-for feature in `ls -v output`
+# Combine the transaction files produced by these sessions
+# into a merged transaction file
+echo "Merging transaction files..."
+for j in `seq $NUMSESSIONS`
 do
-    echo "Comparing test results for $feature"
-    for test in `ls -v output/$feature`
-    do
-        # Check first that the test is actually supposed to output a file
-        if [ -f expected/$feature/$test/transactions.txt ]; then
-            # Check if the output is the same as the expected output
-            OUTPUT="output/$feature/$test/transactions.txt"
-            EXPECT="expected/$feature/$test/transactions.txt"
-
-            # Check and make sure the test output a file, as it was supposed to
-            if [ -f output/$feature/$test/transactions.txt ]; then
-                diff -q --strip-trailing-cr $OUTPUT $EXPECT>/dev/null
-                EXITCODE=$?
-                if [ $EXITCODE -ne 0 ]; then
-                    RES="Test failed - difference detected in output."
-                    ((failed++))
-                else
-                    RES="Test passed."
-                    ((passed++))
-                fi
-            else
-                RES="Test failed - it did not generate an output file."
-                ((failed++))
-            fi
-        else
-            # Check that the test run also didn't create the file
-            if [ -f output/$feature/$test/transactions.txt ]; then
-                RES="Test failed - it was not supposed to generate an output file."
-                ((failed++))
-            else
-                RES="Test passed."
-                ((passed++))
-            fi
-        fi
-        printf "%-35s - %10s\n" $test "$RES"
-    done
-    echo ""
+    echo "`cat "$OUTDIR/transaction$j.txt"`" >> "$OUTDIR/mergedtransactions.txt"
 done
+echo "EOS 0000000 000 0000000 ***" >> "$OUTDIR/mergedtransactions.txt"
+echo "Done."
 
-echo "Tests passed: $passed --- Tests failed: $failed"
+# Run the backoffice on this new transaction file
+echo "Running backoffice for $DAY..."
+python "$ORIGDIR/src/backend/backend.py" "$OUTDIR/mergedtransactions.txt" "$INDIR/masteraccounts.txt" "$OUTDIR/newmasteraccounts.txt" "$OUTDIR/newaccounts.txt"
+echo "Done."
